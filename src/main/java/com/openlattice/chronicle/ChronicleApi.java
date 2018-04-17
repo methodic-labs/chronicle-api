3ebe79a358143bb22b2de18337e012f1720d37ea
Marie Reed <marie@openlattice.com>
2018-04-16 19:07:11 -0700

    Added to api.

--- src/main/java/com/openlattice/chronicle/ChronicleApi.java
+++ src/main/java/com/openlattice/chronicle/ChronicleApi.java
@@ -1,6 +1,8 @@
 package com.openlattice.chronicle;
 
- import retrofit2.http.*;
+import retrofit2.http.*;
+import java.util.UUID;
+
 
 public interface ChronicleApi {
 
@@ -8,12 +10,46 @@ public interface ChronicleApi {
     String CONTROLLER               = "/data";
     String BASE                     = SERVICE + CONTROLLER;
 
+    String ID                       = "id";
     String STUDY_ID                 = "studyId";
     String PARTICIPANT_ID           = "participantId";
+    String ANDROID_ID                 = "androidId";
 
-    String STUDY_ID_PATH            = "/{" + STUDY_ID + "}";
-    String PARTICIPANT_ID_PATH      = "/{" + PARTICIPANT_ID + "}";
+    String ID_PATH              = "/{" + ID + "}";
+    String STUDY_PATH            = "/{" + STUDY_ID + "}";
+    String PARTICIPANT_PATH      = "/{" + PARTICIPANT_ID + "}";
 
-    @POST( BASE + STUDY_ID_PATH + PARTICIPANT_ID_PATH)
+//  sends phone data to openlattice via app, should this be PUT because it'll add not create?
+    @POST( BASE + STUDY_PATH + PARTICIPANT_PATH )
     void logData( @Path( STUDY_ID ) String studyId, @Path( PARTICIPANT_ID ) String participantId );
+
+//  creates a study via web
+    @POST( BASE + STUDY_PATH )
+    UUID createStudy( @Body Study study);
+
+//  creates a participant via web
+    @POST( BASE + PARTICIPANT_PATH )
+    UUID createParticipant( @Body Person participant);
+
+//  enrolls android device to participant via app
+    @POST( BASE + STUDY_PATH + PARTICIPANT_PATH + ANDROID_ID )
+    String enrollDevice( @Path( STUDY_ID) UUID studyId,
+                       @Path( PARTICIPANT_ID ) UUID participantId,
+                       @Path( ANDROID_ID ) String androidId);
+
+//  gets all participants from all studies and non-associated
+    @GET( BASE + PARTICIPANT_PATH )
+    Iterable<Person> getAllParticipants();
+
+//  gets all participants from a study
+    @GET( BASE + STUDY_PATH + PARTICIPANT_PATH)
+    Person getParticipantsFromStudy( @Path( PARTICIPANT_ID ) UUID participantId);
+
+//  gets all studies
+    @GET( BASE + STUDY_PATH )
+    Iterable<Study> getAllStudies();
+
+//  gets a study
+    @GET( BASE + STUDY_PATH + ID_PATH)
+    Study getStudyById( @Path( STUDY_ID ) UUID studyId );
 }
