44784cb197ff504b6619253ee0a15d24dc2cd15a
Marie Reed <marie@openlattice.com>
2018-04-17 15:32:49 -0700

    Added api endpoints.

--- src/main/java/com/openlattice/chronicle/ChronicleApi.java
+++ src/main/java/com/openlattice/chronicle/ChronicleApi.java
@@ -1,7 +1,10 @@
 package com.openlattice.chronicle;
+//import com.openlattice.edm.requests.MetadataUpdate;
+
 
 import retrofit2.http.*;
 import java.util.UUID;
+import java.util.Set;
 
 // TODO: Create Study object and import Study and Person
 
@@ -11,6 +14,9 @@ public interface ChronicleApi {
     String CONTROLLER               = "/data";
     String BASE                     = SERVICE + CONTROLLER;
 
+    String INSTALL_PATH             = "/install";
+    String UPDATE_PATH              = "/update";
+    String BULK_PATH                = "/bulk";
     String STUDY_PATH               = "study";
     String PARTICIPANT_PATH         = "participant";
 
@@ -21,7 +27,7 @@ public interface ChronicleApi {
     String STUDY_ID_PATH            = "/{" + STUDY_ID + "}";
     String PARTICIPANT_ID_PATH      = "/{" + PARTICIPANT_ID + "}";
 
-//  sends phone data to openlattice via app, should this be PUT because it'll add not create?
+//  sends phone data to openlattice, should this be PUT because it'll add not create?
     @POST( BASE + STUDY_ID_PATH + PARTICIPANT_ID_PATH )
     void logData( @Path( STUDY_ID ) String studyId, @Path( PARTICIPANT_ID ) String participantId );
 
@@ -31,9 +37,13 @@ public interface ChronicleApi {
                          @Path( PARTICIPANT_ID ) UUID participantId,
                          @Path( ANDROID_ID ) String androidId);
 
-//  creates a study via web
+//  creates a study
     @POST( BASE + STUDY_PATH )
     UUID createStudy( @Body Study study);
+
+//  deletes a study
+    @DELETE( BASE + STUDY_ID_PATH)
+    void deleteStudy( @Path( STUDY_ID ) UUID studyId);
 
 //  gets all studies
     @GET( BASE + STUDY_PATH )
@@ -43,9 +53,13 @@ public interface ChronicleApi {
     @GET( BASE + STUDY_PATH + STUDY_ID_PATH )
     Study getStudyById( @Path( STUDY_ID ) UUID studyId );
 
-//  creates a participant via web
+//  creates a participant
     @POST( BASE + PARTICIPANT_PATH )
     UUID createParticipant( @Body Person participant);
+
+//  deletes a participant
+    @DELETE( BASE + PARTICIPANT_ID_PATH)
+    void deleteParticipant( @Path( PARTICIPANT_ID ) UUID participantId);
 
 //  gets all participants from all studies and non-associated
     @GET( BASE + PARTICIPANT_PATH )
@@ -54,4 +68,38 @@ public interface ChronicleApi {
 //  gets all participants from a study
     @GET( BASE + STUDY_ID_PATH + PARTICIPANT_PATH )
     Person getParticipantsFromStudy( @Path( STUDY_ID ) UUID studyId);
+
+//  gets participant by id
+    @GET( BASE + PARTICIPANT_ID_PATH)
+    Person getParticipantById( @Path( PARTICIPANT_ID ) UUID participantId);
+
+//  updates a participant's metadata
+    @PATCH( BASE + UPDATE_PATH + PARTICIPANT_ID_PATH)
+    void updateParticipantMetadata( @Path( PARTICIPANT_ID ) UUID participantId,
+                                    @Body MetadataUpdate metadataUpdate);
+
+//  updates a study's metadata
+    @PATCH( BASE + UPDATE_PATH + STUDY_ID_PATH)
+    void updateStudyMetadata( @Path( STUDY_ID ) UUID studyId,
+                              @Body MetadataUpdate metadataUpdate);
+
+//  adds a participant to a study
+    @POST( BASE + INSTALL_PATH + STUDY_ID_PATH + PARTICIPANT_ID_PATH)
+    void addParticipantToStudy( @Path( STUDY_ID )UUID studyID,
+                                @Path( PARTICIPANT_ID ) UUID participantId);
+
+//  removes a participant from a study
+    @DELETE( BASE + INSTALL_PATH + STUDY_ID_PATH + PARTICIPANT_ID_PATH)
+    void removeParticipantFromStudy( @Path( STUDY_ID )UUID studyID,
+                                     @Path( PARTICIPANT_ID ) UUID participantId);
+
+//  adds bulk of participants to a study
+    @POST( BASE + INSTALL_PATH + STUDY_ID_PATH + BULK_PATH)
+    void addParticipantsToStudy( @Path( STUDY_ID )UUID studyID,
+                                 @Body Set<UUID> participantIds);
+
+//  removes bulk of participants from a study
+    @DELETE( BASE + INSTALL_PATH + STUDY_ID_PATH + BULK_PATH)
+    void removeParticipantsFromStudy( @Path( STUDY_ID )UUID studyID,
+                                      @Body Set<UUID> participantIds);
 }
