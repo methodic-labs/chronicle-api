a744381818863b857415ada28fd99324dd37ee03
Marie Reed <marie@openlattice.com>
2018-04-17 10:01:42 -0700

    Modified API endpoints.

--- src/main/java/com/openlattice/chronicle/ChronicleApi.java
+++ src/main/java/com/openlattice/chronicle/ChronicleApi.java
@@ -3,6 +3,7 @@ package com.openlattice.chronicle;
 import retrofit2.http.*;
 import java.util.UUID;
 
+// TODO: Create Study object and import Study and Person
 
 public interface ChronicleApi {
 
@@ -10,46 +11,48 @@ public interface ChronicleApi {
     String CONTROLLER               = "/data";
     String BASE                     = SERVICE + CONTROLLER;
 
-    String ID                       = "id";
+    String STUDY_PATH               = "study";
+    String PARTICIPANT_PATH         = "participant";
+
     String STUDY_ID                 = "studyId";
     String PARTICIPANT_ID           = "participantId";
     String ANDROID_ID                 = "androidId";
 
     String ID_PATH              = "/{" + ID + "}";
-    String STUDY_PATH            = "/{" + STUDY_ID + "}";
-    String PARTICIPANT_PATH      = "/{" + PARTICIPANT_ID + "}";
+    String STUDY_ID_PATH            = "/{" + STUDY_ID + "}";
+    String PARTICIPANT_ID_PATH      = "/{" + PARTICIPANT_ID + "}";
 
 //  sends phone data to openlattice via app, should this be PUT because it'll add not create?
-    @POST( BASE + STUDY_PATH + PARTICIPANT_PATH )
+    @POST( BASE + STUDY_ID_PATH + PARTICIPANT_ID_PATH )
     void logData( @Path( STUDY_ID ) String studyId, @Path( PARTICIPANT_ID ) String participantId );
+
+//  enrolls android device to participant via app
+    @POST( BASE + STUDY_ID_PATH + PARTICIPANT_ID_PATH + ANDROID_ID )
+    String enrollDevice( @Path( STUDY_ID) UUID studyId,
+                         @Path( PARTICIPANT_ID ) UUID participantId,
+                         @Path( ANDROID_ID ) String androidId);
 
 //  creates a study via web
     @POST( BASE + STUDY_PATH )
     UUID createStudy( @Body Study study);
-
-//  creates a participant via web
-    @POST( BASE + PARTICIPANT_PATH )
-    UUID createParticipant( @Body Person participant);
-
-//  enrolls android device to participant via app
-    @POST( BASE + STUDY_PATH + PARTICIPANT_PATH + ANDROID_ID )
-    String enrollDevice( @Path( STUDY_ID) UUID studyId,
-                       @Path( PARTICIPANT_ID ) UUID participantId,
-                       @Path( ANDROID_ID ) String androidId);
-
-//  gets all participants from all studies and non-associated
-    @GET( BASE + PARTICIPANT_PATH )
-    Iterable<Person> getAllParticipants();
-
-//  gets all participants from a study
-    @GET( BASE + STUDY_PATH + PARTICIPANT_PATH)
-    Person getParticipantsFromStudy( @Path( PARTICIPANT_ID ) UUID participantId);
 
 //  gets all studies
     @GET( BASE + STUDY_PATH )
     Iterable<Study> getAllStudies();
 
 //  gets a study
-    @GET( BASE + STUDY_PATH + ID_PATH)
+    @GET( BASE + STUDY_PATH + STUDY_ID_PATH )
     Study getStudyById( @Path( STUDY_ID ) UUID studyId );
+
+//  creates a participant via web
+    @POST( BASE + PARTICIPANT_PATH )
+    UUID createParticipant( @Body Person participant);
+
+//  gets all participants from all studies and non-associated
+    @GET( BASE + PARTICIPANT_PATH )
+    Iterable<Person> getAllParticipants();
+
+//  gets all participants from a study
+    @GET( BASE + STUDY_ID_PATH + PARTICIPANT_PATH )
+    Person getParticipantsFromStudy( @Path( STUDY_ID ) UUID studyId);
 }
