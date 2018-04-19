e80e3d49beee7790c58b404e3b1af6974f369501
Marie Reed <marie@openlattice.com>
2018-04-19 09:59:18 -0700

    Removed Study.java and Device.java because not needed and removed api endpoints that will be handled by dataApi.

--- src/main/java/com/openlattice/chronicle/ChronicleApi.java
+++ src/main/java/com/openlattice/chronicle/ChronicleApi.java
@@ -1,25 +1,13 @@
 package com.openlattice.chronicle;
 
-//import com.openlattice.edm.requests.MetadataUpdate;
-//TODO: Import Study and Person and Device
-
 import retrofit2.http.*;
 import java.util.UUID;
-import java.util.Set;
-
-// TODO: Create Study object and import Study and Person
 
 public interface ChronicleApi {
 
     String SERVICE                  = "/server";
     String CONTROLLER               = "/data";
     String BASE                     = SERVICE + CONTROLLER;
-
-    String INSTALL_PATH             = "/install";
-    String UPDATE_PATH              = "/update";
-    String BULK_PATH                = "/bulk";
-    String STUDY_PATH               = "study";
-    String PARTICIPANT_PATH         = "participant";
 
     String STUDY_ID                 = "studyId";
     String PARTICIPANT_ID           = "participantId";
@@ -37,70 +25,4 @@ public interface ChronicleApi {
     void enrollDevice( @Path( STUDY_ID) UUID studyId,
                          @Path( PARTICIPANT_ID ) UUID participantId,
                          @Path( DEVICE_ID ) String deviceId);
-
-//  creates a study
-    @POST( BASE + STUDY_PATH )
-    UUID createStudy( @Body Study study );
-
-//  deletes a study
-    @DELETE( BASE + STUDY_ID_PATH)
-    void deleteStudy( @Path( STUDY_ID ) UUID studyId );
-
-//  gets all studies
-    @GET( BASE + STUDY_PATH )
-    Iterable<Study> getAllStudies();
-
-//  gets a study
-    @GET( BASE + STUDY_PATH + STUDY_ID_PATH )
-    Study getStudyById( @Path( STUDY_ID ) UUID studyId );
-
-//  creates a participant
-    @POST( BASE + PARTICIPANT_PATH )
-    UUID createParticipant( @Body Person participant );
-
-//  deletes a participant
-    @DELETE( BASE + PARTICIPANT_ID_PATH)
-    void deleteParticipant( @Path( PARTICIPANT_ID ) UUID participantId );
-
-//  gets all participants from all studies and non-associated
-    @GET( BASE + PARTICIPANT_PATH )
-    Iterable<Person> getAllParticipants();
-
-//  gets all participants from a study
-    @GET( BASE + STUDY_ID_PATH + PARTICIPANT_PATH )
-    Iterable<Person> getParticipantsFromStudy( @Path( STUDY_ID ) UUID studyId );
-
-//  gets participant by id
-    @GET( BASE + PARTICIPANT_ID_PATH)
-    Person getParticipantById( @Path( PARTICIPANT_ID ) UUID participantId );
-
-//  updates a participant's metadata
-    @PATCH( BASE + UPDATE_PATH + PARTICIPANT_ID_PATH)
-    void updateParticipantMetadata( @Path( PARTICIPANT_ID ) UUID participantId,
-                                    @Body MetadataUpdate metadataUpdate );
-
-//  updates a study's metadata
-    @PATCH( BASE + UPDATE_PATH + STUDY_ID_PATH)
-    void updateStudyMetadata( @Path( STUDY_ID ) UUID studyId,
-                              @Body MetadataUpdate metadataUpdate );
-
-//  adds a participant to a study
-    @POST( BASE + INSTALL_PATH + STUDY_ID_PATH + PARTICIPANT_ID_PATH)
-    void addParticipantToStudy( @Path( STUDY_ID )UUID studyID,
-                                @Path( PARTICIPANT_ID ) UUID participantId );
-
-//  removes a participant from a study
-    @DELETE( BASE + INSTALL_PATH + STUDY_ID_PATH + PARTICIPANT_ID_PATH)
-    void removeParticipantFromStudy( @Path( STUDY_ID )UUID studyID,
-                                     @Path( PARTICIPANT_ID ) UUID participantId );
-
-//  adds bulk of participants to a study
-    @POST( BASE + INSTALL_PATH + STUDY_ID_PATH + BULK_PATH)
-    void addParticipantsToStudy( @Path( STUDY_ID )UUID studyID,
-                                 @Body Set<UUID> participantIds );
-
-//  removes bulk of participants from a study
-    @DELETE( BASE + INSTALL_PATH + STUDY_ID_PATH + BULK_PATH)
-    void removeParticipantsFromStudy( @Path( STUDY_ID )UUID studyID,
-                                      @Body Set<UUID> participantIds );
 }
