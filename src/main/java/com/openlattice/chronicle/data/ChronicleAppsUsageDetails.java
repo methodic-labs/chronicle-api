8002b19b0c3ef26fdd632cf2c100471c4529676e
Alfonce Nzioka <=>
2020-02-13 16:06:42 -0800

    implement toString(), hashCode(), equals()

--- src/main/java/com/openlattice/chronicle/data/ChronicleAppsUsageDetails.java
+++ src/main/java/com/openlattice/chronicle/data/ChronicleAppsUsageDetails.java
@@ -1,8 +1,11 @@
 package com.openlattice.chronicle.data;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import org.apache.olingo.commons.api.edm.FullQualifiedName;
 
 import java.util.Map;
+import java.util.Objects;
 import java.util.Set;
 
 /**
@@ -13,19 +16,44 @@ public class ChronicleAppsUsageDetails {
     private final Map<FullQualifiedName, Set<Object>> entityDetails;
     private final Map<FullQualifiedName, Set<Object>> associationDetails;
 
+    @JsonCreator
     public ChronicleAppsUsageDetails(
-            Map<FullQualifiedName, Set<Object>> entityDetails,
-            Map<FullQualifiedName, Set<Object>> associationDetails
+            @JsonProperty( SerializationConstants.ENTITY_DETAILS ) Map<FullQualifiedName, Set<Object>> entityDetails,
+            @JsonProperty( SerializationConstants.ASSOCIATION_DETAILS )
+                    Map<FullQualifiedName, Set<Object>> associationDetails
     ) {
         this.entityDetails = entityDetails;
         this.associationDetails = associationDetails;
     }
 
+    @JsonProperty( SerializationConstants.ENTITY_DETAILS )
     public Map<FullQualifiedName, Set<Object>> getEntityDetails() {
         return entityDetails;
     }
 
+    @JsonProperty( SerializationConstants.ASSOCIATION_DETAILS )
     public Map<FullQualifiedName, Set<Object>> getAssociationDetails() {
         return associationDetails;
+    }
+
+    @Override public boolean equals( Object o ) {
+        if ( this == o )
+            return true;
+        if ( !( o instanceof ChronicleAppsUsageDetails ) )
+            return false;
+        ChronicleAppsUsageDetails chronicleAppsUsageDetails = (ChronicleAppsUsageDetails) o;
+        return Objects.equals( entityDetails, chronicleAppsUsageDetails.entityDetails ) &&
+                Objects.equals( associationDetails, chronicleAppsUsageDetails.associationDetails );
+    }
+
+    @Override public String toString() {
+        return "ChronicleAppsUsageDetails{" +
+                "entityDetails=" + entityDetails +
+                ", associationDetails=" + associationDetails +
+                '}';
+    }
+
+    @Override public int hashCode() {
+        return Objects.hash( entityDetails, associationDetails );
     }
 }
