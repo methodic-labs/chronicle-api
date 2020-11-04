f2e261ba7823fb92c24547138e45102af46b2308
Alfonce Nzioka <alfonce.starfish@gmail.com>
2020-11-03 19:20:25 -0800

    serialization const

--- src/main/java/com/openlattice/chronicle/data/SerializationConstants.java
+++ src/main/java/com/openlattice/chronicle/data/SerializationConstants.java
@@ -5,10 +5,13 @@ package com.openlattice.chronicle.data;
  */
 //serializations for the chronicle apps usage
 public final class SerializationConstants {
-    public static final String ASSOCIATION_DETAILS = "associationDetails";
-    public static final String ENTITY_DETAILS = "entityDetails";
+    public static final String ASSOCIATION_DETAILS   = "associationDetails";
+    public static final String ENTITY_DETAILS        = "entityDetails";
     public static final String QUESTIONNAIRE_DETAILS = "questionnaireDetails";
-    public static final String QUESTIONS = "questions";
+    public static final String QUESTIONS             = "questions";
+    public static final String ENTITY_BY_ENTITY_KEY  = "entityByEntityKey";
+    public static final String EDGE_ENTITY_KEYS      = "edgeEntityKeys";
 
-    private SerializationConstants() {}
+    private SerializationConstants() {
+    }
 }
