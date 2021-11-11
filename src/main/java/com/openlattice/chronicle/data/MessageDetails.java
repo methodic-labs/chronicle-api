c6f15d08ac1d23cb727a2ad1c966e28d8a6f0382
Todd Bergman <todd@openlattice.com>
2021-11-11 09:53:48 -0800

    add MessageDetails type

---
+++ src/main/java/com/openlattice/chronicle/data/MessageDetails.java
@@ -0,0 +1,54 @@
+package com.openlattice.chronicle.data;
+
+import java.awt.*;
+import java.time.OffsetDateTime;
+import java.util.Objects;
+import java.util.UUID;
+
+public class MessageDetails {
+
+    private String          messageType;
+    private OffsetDateTime  dateTime;
+    private String          participantId;
+    private String          phoneNumber;
+    private String          url;
+
+    public MessageDetails(
+            String messageType,
+            OffsetDateTime dateTime,
+            String participantId,
+            String phoneNumber,
+            String url) {
+        this.messageType = messageType;
+        this.dateTime = dateTime;
+        this.participantId = participantId;
+        this.phoneNumber = phoneNumber;
+        this.url = url;
+    }
+
+    public String getMessageType() {
+        return messageType;
+    }
+
+    public OffsetDateTime getDateTime() {
+        return dateTime;
+    }
+
+    public String getParticipantId() {
+        return participantId;
+    }
+
+    public String getPhoneNumber() { return phoneNumber; }
+
+    public String getUrl() { return url; };
+
+    @Override public int hashCode() {
+        return Objects
+                .hash(
+                        messageType,
+                        dateTime,
+                        participantId,
+                        phoneNumber
+                );
+    }
+}
