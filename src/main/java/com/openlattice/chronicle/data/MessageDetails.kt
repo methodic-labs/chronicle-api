bdc5628204cc840d89056cfa47032469cc51b29a
Todd Bergman <todd@openlattice.com>
2021-11-17 12:41:34 -0800

    Add MessageType and MessageDetails

---
+++ src/main/java/com/openlattice/chronicle/data/MessageDetails.kt
@@ -0,0 +1,13 @@
+package com.openlattice.chronicle.data
+
+import java.time.OffsetDateTime
+import java.util.*
+
+data class MessageDetails(
+    val messageType: MessageType,
+    val dateTime: OffsetDateTime,
+    val participantId: String,
+    val phoneNumber: String,
+    val url: String,
+    val studyId: UUID,
+)
