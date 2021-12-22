257214b071dac19133e8d4b152c62710664dee09
Todd Bergman <todd@openlattice.com>
2021-12-21 16:17:21 -0800

    Update src/main/java/com/openlattice/chronicle/api/ChronicleApi.java
    Co-authored-by: Hristo Oskov <UnsungHero97@users.noreply.github.com>

--- src/main/java/com/openlattice/chronicle/api/ChronicleApi.java
+++ src/main/java/com/openlattice/chronicle/api/ChronicleApi.java
@@ -11,7 +11,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.UUID;
-import java.util.concurrent.ExecutionException;
 
 /**
  * @author alfoncenzioka &lt;alfonce@openlattice.com&gt;
