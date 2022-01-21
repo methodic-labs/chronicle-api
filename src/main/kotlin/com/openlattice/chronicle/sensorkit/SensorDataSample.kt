356d0645dddfee21fed4f44c6c77b4b91ffa437e
Alfonce Nzioka <alfonce.starfish@gmail.com>
2022-01-20 23:33:13 -0800

    add sensor kit api

---
+++ src/main/kotlin/com/openlattice/chronicle/sensorkit/SensorDataSample.kt
@@ -0,0 +1,17 @@
+package com.openlattice.chronicle.sensorkit
+
+import java.time.OffsetDateTime
+import java.util.*
+
+/**
+ * @author alfoncenzioka &lt;alfonce@openlattice.com&gt;
+ */
+data class SensorDataSample (
+        val dateRecorded: OffsetDateTime,
+        val startDate: OffsetDateTime,
+        val endDate: OffsetDateTime,
+        val data: List<Map<UUID, String>>,
+        val timezone: String,
+        val id: UUID,
+        val sensorName: String
+)
