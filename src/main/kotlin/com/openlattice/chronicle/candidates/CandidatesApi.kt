a93b1fe898ba83ee599a4dcb0e733a78e3aa9ef2
Hristo Oskov <hristo@openlattice.com>
2022-01-27 12:05:42 -0800

    getCandidate

--- src/main/kotlin/com/openlattice/chronicle/candidates/CandidatesApi.kt
+++ src/main/kotlin/com/openlattice/chronicle/candidates/CandidatesApi.kt
@@ -1,7 +1,9 @@
 package com.openlattice.chronicle.candidates
 
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.Path
 import java.util.UUID
 
 interface CandidatesApi {
@@ -10,8 +12,14 @@ interface CandidatesApi {
         const val SERVICE = "/chronicle"
         const val CONTROLLER = "/candidates"
         const val BASE = SERVICE + CONTROLLER
+
+        const val CANDIDATE_ID_PARAM = "candidateId"
+        const val CANDIDATE_ID_PATH = "/{$CANDIDATE_ID_PARAM}"
     }
 
     @POST(BASE)
     fun registerCandidate(@Body candidate: Candidate): UUID
+
+    @GET(BASE + CANDIDATE_ID_PATH)
+    fun getCandidate(@Path(CANDIDATE_ID_PARAM) candidateId: UUID): Candidate
 }
