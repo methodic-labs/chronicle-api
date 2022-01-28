feb0a72a7c856434f839c3140d497bbc9838d268
Andrew Carter <and.s.carter@gmail.com>
2022-01-28 11:23:14 -0800

    add constants for night downlaods

---
+++ src/main/kotlin/com/openlattice/chronicle/timeusediary/TimeUseDiaryConstants.kt
@@ -0,0 +1,20 @@
+package com.openlattice.chronicle.timeusediary
+
+/**
+ * @author Andrew Carter andrew@openlattice.com
+ */
+
+class TimeUseDiaryConstants {
+    val nightConstants = listOf<String>(
+        "typicalDay",
+        "nonTypicalReason",
+        "wakeUpToday",
+        "dayTimeHours",
+        "typicalSleepPattern",
+        "nonTypicalSleepPattern",
+        "sleepingArrangement",
+        "wakeUpCount",
+        "bgTvNight",
+        "bgAudioNight"
+    )
+}
