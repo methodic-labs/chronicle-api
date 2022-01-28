514f9d4a2eb19b9e20044ce8899fbb9c7891c2f6
Hristo Oskov <hristo@openlattice.com>
2022-01-27 21:35:07 -0800

    not actually sure if candidate needs to extend AbstractSecurableObject

--- src/main/kotlin/com/openlattice/chronicle/candidates/Candidate.kt
+++ src/main/kotlin/com/openlattice/chronicle/candidates/Candidate.kt
@@ -1,22 +1,15 @@
 package com.openlattice.chronicle.candidates
 
-import com.openlattice.chronicle.authorization.AbstractSecurableObject
-import com.openlattice.chronicle.authorization.SecurableObjectType
 import com.openlattice.chronicle.ids.IdConstants
 import java.time.LocalDate
 import java.util.UUID
 
 data class Candidate(
-    var candidateId: UUID = IdConstants.UNINITIALIZED.id,
+    var id: UUID = IdConstants.UNINITIALIZED.id,
     val firstName: String? = null,
     val lastName: String? = null,
     val name: String? = null,
     val dateOfBirth: LocalDate? = null,
     val email: String? = null,
     val phoneNumber: String? = null
-) : AbstractSecurableObject(
-    // title and description are intentionally ignored
-    candidateId, "candidate", "candidate"
-) {
-    override val category: SecurableObjectType = SecurableObjectType.Candidate
-}
+)
