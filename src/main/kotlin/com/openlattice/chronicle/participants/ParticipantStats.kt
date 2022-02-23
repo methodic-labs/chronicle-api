554972362b43b5f62fde1bd035eeb44b92e9d487
Alfonce Nzioka <alfonce.starfish@gmail.com>
2022-02-23 07:18:29 -0800

    add participantId property

--- src/main/kotlin/com/openlattice/chronicle/participants/ParticipantStats.kt
+++ src/main/kotlin/com/openlattice/chronicle/participants/ParticipantStats.kt
@@ -6,6 +6,7 @@ import java.time.OffsetDateTime
  * @author alfoncenzioka &lt;alfonce@openlattice.com&gt;
  */
 data class ParticipantStats(
+    val participantId: String,
     val androidFirstDate: OffsetDateTime? = null,
     val androidLastDate: OffsetDateTime? = null,
     val androidDatesCount: Int = 0,
