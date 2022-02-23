c56d0d6b925a66589d05332fe0b0e8cb663993d7
Alfonce Nzioka <alfonce.starfish@gmail.com>
2022-02-23 06:58:58 -0800

    add participant stats api

---
+++ src/main/kotlin/com/openlattice/chronicle/participants/ParticipantStats.kt
@@ -0,0 +1,18 @@
+package com.openlattice.chronicle.participants
+
+import java.time.OffsetDateTime
+
+/**
+ * @author alfoncenzioka &lt;alfonce@openlattice.com&gt;
+ */
+data class ParticipantStats(
+    val androidFirstDate: OffsetDateTime? = null,
+    val androidLastDate: OffsetDateTime? = null,
+    val androidDatesCount: Int = 0,
+    val iosFirstDate: OffsetDateTime? = null,
+    val iosLastDate: OffsetDateTime? = null,
+    val iosDatesCount: Int = 0,
+    val tudFirsDate: OffsetDateTime? = null,
+    val tudLastDate: OffsetDateTime? = null,
+    val tudDatesCount: Int = 0
+)
