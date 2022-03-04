e1fda57b4eb2fc713ed8f2c3a394394a85f3ef9d
Alfonce Nzioka <alfonce.starfish@gmail.com>
2022-03-03 21:56:32 -0800

    tud component

--- src/main/kotlin/com/openlattice/chronicle/settings/AppComponent.kt
+++ src/main/kotlin/com/openlattice/chronicle/settings/AppComponent.kt
@@ -4,7 +4,10 @@ package com.openlattice.chronicle.settings
  * @author alfoncenzioka &lt;alfonce@openlattice.com&gt;
  */
 enum class AppComponent(private val component: String) {
-    CHRONICLE("chronicle"), CHRONICLE_DATA_COLLECTION("chronicle_data_collection"), CHRONICLE_SURVEYS("chronicle_surveys");
+    CHRONICLE("chronicle"),
+    CHRONICLE_DATA_COLLECTION("chronicle_data_collection"),
+    CHRONICLE_SURVEYS("chronicle_surveys"),
+    TIME_USE_DIARY("time_use_diary");
 
     override fun toString(): String {
         return component
