0aba701f8b5829591d12896fe11f78b7b86b76b0
Matthew Tamayo-Rios <matthew@openlattice.com>
2022-03-30 16:22:01 -0700

    Refactor settings.

--- src/main/kotlin/com/openlattice/chronicle/study/StudySettings.kt
+++ src/main/kotlin/com/openlattice/chronicle/study/StudySettings.kt
@@ -5,6 +5,9 @@ package com.openlattice.chronicle.study
  * @author Matthew Tamayo-Rios &lt;matthew@getmethodic.com&gt;
  */
 class StudySettings(settings : Map<String, StudySetting> = mapOf()) : Map<String, StudySetting>  by settings {
+    init {
+
+    }
     private val h = settings.hashCode()
     override fun equals(other: Any?): Boolean {
         return if (other !is StudySettings) return false
@@ -16,4 +19,10 @@ class StudySettings(settings : Map<String, StudySetting> = mapOf()) : Map<String
     }
 
     override fun hashCode(): Int = h
+}
+
+enum class StudySettingType(val type:String) {
+    DATA_COLLECTION("dataCollection"),
+    SENSOR("sensor"),
+    NOTIFICATIONS("notifications")
 }
