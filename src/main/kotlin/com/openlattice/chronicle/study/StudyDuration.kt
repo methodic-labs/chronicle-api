0a4152c3e1ff8d8e5b755e2027623789dff3be17
Matthew Tamayo-Rios <matthew@openlattice.com>
2022-04-01 18:16:08 -0400

    Add study limits api.

---
+++ src/main/kotlin/com/openlattice/chronicle/study/StudyDuration.kt
@@ -0,0 +1,10 @@
+package com.openlattice.chronicle.study
+
+/**
+ * @author Matthew Tamayo-Rios &lt;matthew@openlattice.com&gt;
+ */
+data class StudyDuration(
+    val years: Short = 0,
+    val months: Short = 0,
+    val days: Short = 0,
+)
---
+++ src/main/kotlin/com/openlattice/chronicle/study/StudyFeature.kt
@@ -0,0 +1,12 @@
+package com.openlattice.chronicle.study
+
+/**
+ *
+ * @author Matthew Tamayo-Rios &lt;matthew@openlattice.com&gt;
+ */
+enum class StudyFeature {
+    APP_USAGE,
+    APP_USAGE_SURVEY,
+    TIME_USE_DIARY,
+    ARCHIVE
+}
