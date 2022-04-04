bb0a4806af8c80c87ba8c3b4449d08263d3cbb63
Matthew Tamayo-Rios <matthew@openlattice.com>
2022-04-04 14:22:24 -0400

    Add legacy components.

--- src/main/kotlin/com/openlattice/chronicle/study/StudyFeature.kt
+++ src/main/kotlin/com/openlattice/chronicle/study/StudyFeature.kt
@@ -8,5 +8,9 @@ enum class StudyFeature {
     APP_USAGE,
     APP_USAGE_SURVEY,
     TIME_USE_DIARY,
-    ARCHIVE
+    ARCHIVE,
+    //Legacy features
+    CHRONICLE_SURVEYS,
+    CHRONICLE,
+    CHRONICLE_DATA_COLLECTION
 }
